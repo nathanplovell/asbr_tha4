aec68c29042a1ebfb219ffb23857a888aef2181b
Nathan Lovell <nathanplovell@gmail.com>
2026-04-23 12:09:35 -0500

    Added test3 and test4 to first test script

--- IK_tool_test.m
+++ IK_tool_test.m
@@ -1,25 +1,27 @@
 close all
 
-test1 = [0 pi/2 0 pi/4 0 pi/6 0];
+test1 = [0 pi/2 0 pi/4 0 pi/6 0]; % This example is outside of the joint limits...
 test2 = [0 pi/2 0 pi/4 0 pi/7 0];
+test3 = [0 0 0 -pi/2 0 0 0]; % Doesn't converge for this one??
+test4 = [0 0 0 -pi/2.1 0 0 0];
 
 
 % plot3(0, 0, 0);
 % view(3)
-T1 = FK_space(panda, test1, true);
+T1 = FK_space(panda, test3, true);
 pause(1)
-T2 = FK_space(panda, test2, true);
+T2 = FK_space(panda, test4, true);
 pause(1)
 % view(3)
 % figure
 
-test_result = IK_tool_position(panda, test1, T2, true)
+[test_result, t_vec_mat] = IK_tool_position(panda, test1, T2, true)
 
 % Visualize the iterations
-% IK_gif(panda,t_vec_mat,'IK_tool_test.gif')
+%IK_gif(panda,t_vec_mat,'IK_tool_position_test1.gif')
 
 % figure
-%test_plot = FK_space(panda, test_result, true);
+test_plot = FK_space(panda, test_result, true);
 
 % Plot first iteration of the optimization
 test_plot2 = FK_space(panda, t_vec_mat(2,:), true)
